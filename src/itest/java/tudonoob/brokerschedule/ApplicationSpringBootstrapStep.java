89c0142a8a142af65a43ee66da78ae73e162f91c
William Ahrons <william.ahrons@gmail.com>
2016-06-21 18:59:50 -0300

    changing features tests to after their execution clear the cache

--- src/itest/java/tudonoob/brokerschedule/ApplicationSpringBootstrapStep.java
+++ src/itest/java/tudonoob/brokerschedule/ApplicationSpringBootstrapStep.java
@@ -1,12 +1,24 @@
 package tudonoob.brokerschedule;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
+import org.springframework.beans.factory.annotation.Autowired;
+import tudonoob.brokerschedule.controller.BrokerController;
 
 
 public class ApplicationSpringBootstrapStep extends BrokerScheduleApplicationTests {
 
+    @Autowired
+    private BrokerController controller;
+
+    @After
+    public void clearCache() {
+        controller.clearCache();
+    }
+
     @Given("I bootstrap the spring application")
-    public void bootstrapApplication(){
+    public void bootstrapApplication() {
 
     }
+
 }
--- src/itest/java/tudonoob/brokerschedule/FeatureTestRunner.java
+++ src/itest/java/tudonoob/brokerschedule/FeatureTestRunner.java
@@ -1,10 +1,19 @@
 package tudonoob.brokerschedule;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.cs.A;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import tudonoob.brokerschedule.controller.BrokerController;
+import tudonoob.brokerschedule.domain.Broker;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(format = "pretty", features = "src/itest/resources")
 public class FeatureTestRunner {
+
+
+
 }
