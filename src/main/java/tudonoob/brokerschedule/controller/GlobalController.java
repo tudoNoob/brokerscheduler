b75435968d9989f81e70abc7c899ee077a13c666
William Ahrons <william.ahrons@gmail.com>
2016-07-01 19:23:33 -0300

    adding error handling for badRequest

--- src/main/java/tudonoob/brokerschedule/controller/GlobalController.java
+++ src/main/java/tudonoob/brokerschedule/controller/GlobalController.java
@@ -15,7 +15,21 @@ public class GlobalController {
     @ExceptionHandler(BrokerScheduleException.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage handleError(BrokerScheduleException exception) {
-        return new ErrorMessage(exception.getMessage(), exception.getExceptionName(),
-                HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildErrorMessage(exception, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ErrorMessage buildErrorMessage(Exception exception, HttpStatus status) {
+        if (exception instanceof BrokerScheduleException) {
+            return new ErrorMessage(exception.getMessage(), ((BrokerScheduleException) exception).getExceptionName(),
+                    status);
+        }
+        return new ErrorMessage(exception.getMessage(), exception.getClass().getName(),
+                status);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleDomainBuildError(IllegalAccessException exception) {
+        return buildErrorMessage(exception, HttpStatus.BAD_REQUEST);
     }
 }
